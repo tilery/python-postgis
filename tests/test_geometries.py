84e43c23db822bf997d5901fdd7811ae007e51a0
Yohan Boniface <yb@enix.org>
2015-11-25 08:57:02 +0100

    Use WHERE clause in tests, to test casting again

--- tests/test_geometries.py
+++ tests/test_geometries.py
@@ -8,9 +8,9 @@ from postgis import Point, LineString, Polygon
     (-1.123456789, 2.987654321),
 ])
 def test_point_should_round(cursor, expected):
-    cursor.execute('INSERT INTO points (geom) VALUES (%s)',
-                   [Point(*expected, srid=4326)])
-    cursor.execute('SELECT geom FROM points LIMIT 1')
+    params = [Point(*expected, srid=4326)]
+    cursor.execute('INSERT INTO points (geom) VALUES (%s)', params)
+    cursor.execute('SELECT geom FROM points WHERE geom=%s', params)
     geom = cursor.fetchone()[0]
     assert geom.coords == expected
 
@@ -19,9 +19,9 @@ def test_point_should_round(cursor, expected):
     ((30, 10), (10, 30), (40, 40)),
 ])
 def test_linestring_should_round(cursor, expected):
-    cursor.execute('INSERT INTO linestrings (geom) VALUES (%s)',
-                   [LineString(expected, srid=4326)])
-    cursor.execute('SELECT geom FROM linestrings LIMIT 1')
+    params = [LineString(expected, srid=4326)]
+    cursor.execute('INSERT INTO linestrings (geom) VALUES (%s)', params)
+    cursor.execute('SELECT geom FROM linestrings WHERE geom=%s', params)
     geom = cursor.fetchone()[0]
     assert geom.coords == expected
 
@@ -30,9 +30,9 @@ def test_linestring_should_round(cursor, expected):
     (((35, 10), (45, 45), (15, 40), (10, 20), (35, 10)), ((20, 30), (35, 35), (30, 20), (20, 30))),  # noqa
 ])
 def test_polyggon_should_round(cursor, expected):
-    cursor.execute('INSERT INTO polygons (geom) VALUES (%s)',
-                   [Polygon(expected, srid=4326)])
-    cursor.execute('SELECT geom FROM polygons LIMIT 1')
+    params = [Polygon(expected, srid=4326)]
+    cursor.execute('INSERT INTO polygons (geom) VALUES (%s)', params)
+    cursor.execute('SELECT geom FROM polygons WHERE geom=%s', params)
     geom = cursor.fetchone()[0]
     assert geom.coords == expected
 
