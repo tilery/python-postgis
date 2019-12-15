23ede13afe91378dd7b64a6a93c69c8d54a00518
Yohan Boniface <yb@enix.org>
2019-12-15 10:49:26 +0100

    Packaging cleaning o

---
+++ Makefile
@@ -0,0 +1,4 @@
+develop:
+	pip install -e .[test]
+test:
+	py.test -vvx
--- setup.py
+++ setup.py
@@ -1,77 +1,65 @@
 """Pyscopg and asyncpg helpers to work with PostGIS."""
 
-import glob
-from setuptools import setup, find_packages, Extension
-from codecs import open  # To use a consistent encoding
-from os import path
 import sys
+from pathlib import Path
 
-HERE = path.abspath(path.dirname(__file__))
-
-# Get the long description from the relevant file
-with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
-
-
-def is_pkg(line):
-    return line and not line.startswith(('--', 'git', '#'))
+from setuptools import Extension, find_packages, setup
 
 
 def list_modules(dirname):
-    filenames = glob.glob(path.join(dirname, '*.py'))
-
-    module_names = []
-    for name in filenames:
-        module, ext = path.splitext(path.basename(name))
-        if module != '__init__':
-            module_names.append(module)
-
-    return module_names
+    paths = Path(dirname).glob("*.py")
+    return [p.stem for p in paths if p.stem != "__init__"]
 
 
 try:
     from Cython.Distutils import build_ext
+
     CYTHON = True
 except ImportError:
-    sys.stdout.write('\nNOTE: Cython not installed. python-postgis will '
-                     'still work fine, but may run a bit slower.\n\n')
+    sys.stdout.write(
+        "\nNOTE: Cython not installed. python-postgis will "
+        "still work fine, but may run a bit slower.\n\n"
+    )
     CYTHON = False
     cmdclass = {}
     ext_modules = []
 else:
     ext_modules = [
-        Extension('postgis.' + ext, [path.join('postgis', ext + '.py')])
-        for ext in list_modules(path.join(HERE, 'postgis'))]
+        Extension("postgis." + ext, [str(Path("postgis") / f"{ext}.py")])
+        for ext in list_modules(Path("postgis"))
+    ]
 
-    cmdclass = {'build_ext': build_ext}
+    cmdclass = {"build_ext": build_ext}
 
 
 VERSION = (1, 0, 4)
 
 setup(
-    name='postgis',
+    name="postgis",
     version=".".join(map(str, VERSION)),
     description=__doc__,
-    long_description=long_description,
-    url="https://github.com/yohanboniface/python-postgis",
-    author='Yohan Boniface',
-    author_email='yohan.boniface@data.gouv.fr',
-    license='WTFPL',
-
+    long_description=Path("README.md").read_text(),
+    url="https://github.com/tilery/python-postgis",
+    author="Yohan Boniface",
+    author_email="yohanboniface@free.fr",
+    license="WTFPL",
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        'Development Status :: 4 - Beta',
-
-        'Intended Audience :: Developers',
-        'Topic :: Scientific/Engineering :: GIS',
-
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Scientific/Engineering :: GIS",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
-    keywords='psycopg postgis gis asyncpg',
-    packages=find_packages(exclude=['tests']),
-    extras_require={'test': ['pytest'], 'docs': 'mkdocs'},
+    keywords="psycopg postgis gis asyncpg",
+    packages=find_packages(exclude=["tests"]),
+    extras_require={
+        "test": ["pytest", "pytest-asyncio", "psycopg2", "asyncpg"],
+        "docs": "mkdocs",
+    },
     include_package_data=True,
     cmdclass=cmdclass,
     ext_modules=ext_modules,
